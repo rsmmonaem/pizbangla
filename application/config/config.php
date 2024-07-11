b056ed4980e0a622d8caf87da3d264746259f32d
MOHAMMAD MUNAYAM SOWDAGOR <72827332+rsmmonaem@users.noreply.github.com>
2024-07-11 16:46:42 +0600

    cds

--- application/config/config.php
+++ application/config/config.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('Asia/Dhaka');
 
 
- $config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/PizBangla/';
+ $config['base_url'] = 'https://'.$_SERVER['SERVER_NAME'].'/PizBangla/';
 // $config['base_url'] = 'https://nibiz.online/PizBangla/';
 
 
